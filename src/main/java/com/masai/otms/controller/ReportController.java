25d6ff63ca84a3ac92691e1db39522e05b4c6e58
DikshaGaupale <113422182+DikshaGaupale@users.noreply.github.com>
2023-05-06 16:56:19 +0530

    day3 done

--- src/main/java/com/masai/otms/controller/ReportController.java
+++ src/main/java/com/masai/otms/controller/ReportController.java
@@ -1,9 +1,54 @@
 package com.masai.otms.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.otms.exceptions.ReportException;
+import com.masai.otms.models.Report;
+import com.masai.otms.service.ReportService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin("*")
 public class ReportController {
+	
+	@Autowired
+	private ReportService reportSer;
+	@PostMapping("/reports")
+	public ResponseEntity<Report>  addPackageHandler(@Valid @RequestBody Report reprt) throws ReportException {
+         
+		return new ResponseEntity<>(reportSer.addReport(reprt),HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/reports/{reportId}")
+	public ResponseEntity<Report> deletePackageByIdHandler(@PathVariable @RequestBody Integer reportId) throws ReportException {
+
+		return new ResponseEntity<>(reportSer.deleteReportById(reportId),HttpStatus.OK);
+	}
+
+	@GetMapping("/reports/{reportId}")
+	public ResponseEntity<List<Report>> searchReportByIdHandler(@PathVariable Integer reportId) throws ReportException {
+	
+		
+		return new ResponseEntity<>(reportSer.viewAllReportsById(reportId),HttpStatus.OK);
+		
+	}
+
+	@GetMapping("/reports")
+	public ResponseEntity<List<Report>> viewAllPackagesHandler() throws ReportException {
+	
+		
+		return new ResponseEntity<>(reportSer.viewAllReports(),HttpStatus.OK);
+	}
 }
--- src/main/java/com/masai/otms/models/Packages.java
+++ src/main/java/com/masai/otms/models/Packages.java
@@ -38,60 +38,7 @@ public class Packages {
     @Embedded
     private PaymentDetails paymentDetails;
 
-	public Integer getPackageId() {
-		return packageId;
-	}
-
-	public void setPackageId(Integer packageId) {
-		this.packageId = packageId;
-	}
-
-	public String getPackageName() {
-		return packageName;
-	}
-
-	public void setPackageName(String packageName) {
-		this.packageName = packageName;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getPackageType() {
-		return packageType;
-	}
-
-	public void setPackageType(String packageType) {
-		this.packageType = packageType;
-	}
-
-	public Double getPaymentCost() {
-		return paymentCost;
-	}
-
-	public void setPaymentCost(Double paymentCost) {
-		this.paymentCost = paymentCost;
-	}
-
-	public PaymentDetails getPaymentDetails() {
-		return paymentDetails;
-	}
-
-	public void setPaymentDetails(PaymentDetails paymentDetails) {
-		this.paymentDetails = paymentDetails;
-	}
-
-	@Override
-	public String toString() {
-		return "Packages [packageId=" + packageId + ", packageName=" + packageName + ", description=" + description
-				+ ", packageType=" + packageType + ", paymentCost=" + paymentCost + ", paymentDetails=" + paymentDetails
-				+ "]";
-	}
+	
 
 	
     
