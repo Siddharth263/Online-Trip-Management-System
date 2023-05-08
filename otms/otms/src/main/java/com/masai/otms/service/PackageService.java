0f9bd73359a4843e9ce079224a2b1cf2a45ea09c
Siddharth263 <priyanshulal2213@gmail.com>
2023-05-08 12:55:22 +0530

    resolved report and package controller and service issue

--- otms/otms/src/main/java/com/masai/otms/service/PackageService.java
+++ otms/otms/src/main/java/com/masai/otms/service/PackageService.java
@@ -1,4 +1,12 @@
 package com.masai.otms.service;
 
+import com.masai.otms.models.Package;
+
+import java.util.List;
+
 public interface PackageService {
+    Package addPackage(Package p);
+    String deletePackage(Integer id);
+    Package searchPackage(Integer id);
+    List<Package> viewAllPackage();
 }
